11c4ee22d43c527b73fb253f9e39fe459379905d
Maximilian Maroe <drakfyre@gmail.com>
2023-11-18 08:20:19 -0800

    Add project files.

---
+++ DereferencingStringLiteralHelloWorld/DereferencingStringLiteralHelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+    cout << *"Heya! This is an ";
+    cout << *"example of using";
+    cout << *"le dereference operator! This";
+    cout << *"lets you easily access the first";
+    cout << *"ordinal of a string.";
+    cout << *" What a wonderful";
+    cout <<   "world!";
+}
